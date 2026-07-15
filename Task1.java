d0882720ecc14837b9ef7b70cd997de8b89fe9c1
Volckmann, Patrick Simon <60431224+pharadox1500@users.noreply.github.com>
2026-07-15 16:25:24 +0800

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,1 +1,10 @@
-
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+  
+    }
+}
