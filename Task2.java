abd9c07dc2e24b9d7dd7393ffb3c80d5c9a4d85a
Volckmann, Patrick Simon <60431224+pharadox1500@users.noreply.github.com>
2026-07-23 16:09:05 +0800

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,1 +1,20 @@
+public class Task2 {
 
+    public static void main(String[] args) {
+
+        char letter = 'H';
+        int num = 3;
+        char l = 'l';
+        byte zero = 0;
+        String end = "rld";
+        short whole = 2;
+        float decimal = 0.0f;
+        boolean check = true;
+
+        String output = letter + "" + num + l + l + zero
+                + " w" + zero + end + " "
+                + whole + decimal + " " + check;
+
+        System.out.println(output);
+    }
+}
