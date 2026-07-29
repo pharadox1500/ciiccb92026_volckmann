573e01b47455957ff6e9c7f53c10450be17d0886
Volckmann, Patrick Simon <60431224+pharadox1500@users.noreply.github.com>
2026-07-29 14:55:24 +0800

    Create Task5.java

---
+++ Task5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = input.nextInt();
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal");
+        } else {
+            int largest = num1;
+
+            if (num2 > largest) {
+                largest = num2;
+            }
+
+            if (num3 > largest) {
+                largest = num3;
+            }
+
+            System.out.println("The largest number is: " + largest);
+        }
+
+        input.close();
+    }
+}
