ccc682bd9f8b1dba0033d873a4bb2c3870cbecda
Volckmann, Patrick Simon <60431224+pharadox1500@users.noreply.github.com>
2026-08-25 21:19:16 +0800

    Update Task131.java

--- Task131.java
+++ Task131.java
@@ -1,20 +1,17 @@
 package Task13;
-
-class Afritada {
- public void showIngredients(){System.out.println("Afritada = [Tomato Sauce, Meat]");}
-}
-class Mechado extends Afritada {
- public void showIngredients(){System.out.println("Mechado = [Tomato Sauce, Meat, Potatoes & Carrots]");}
-}
-class Menudo extends Afritada {
- public void showIngredients(){System.out.println("Menudo = [Tomato Sauce, Meat, Potatoes & Carrots, Liver Spread, Raisins, Hotdog]");}
-}
-class Caldereta extends Afritada {
- public void showIngredients(){System.out.println("Caldereta = [Tomato Sauce, Meat, Potatoes & Carrots, Tomato Paste, Liver Spread, Raisins, Hotdog, Cheese]");}
-}
-public class Task131 {
- public static void main(String[] args) {
-  Afritada meal1=new Afritada(), meal2=new Mechado(), meal3=new Menudo(), meal4=new Caldereta();
-  meal1.showIngredients(); meal2.showIngredients(); meal3.showIngredients(); meal4.showIngredients();
+class Afritada{public void showIngredients(){System.out.println("Afritada");}}
+class Mechado extends Afritada{public void showIngredients(){System.out.println("Mechado");}}
+class Menudo extends Afritada{public void showIngredients(){System.out.println("Menudo");}}
+class Caldereta extends Afritada{public void showIngredients(){System.out.println("Caldereta");}}
+public class Task131{
+ public static void main(String[] args){
+  Afritada meal1=new Afritada();
+  Afritada meal2=new Mechado();
+  Afritada meal3=new Menudo();
+  Afritada meal4=new Caldereta();
+  meal1.showIngredients();
+  meal2.showIngredients();
+  meal3.showIngredients();
+  meal4.showIngredients();
  }
 }
