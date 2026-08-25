5328997d4e35c46ba15f202dba9bc5071a6f9097
Volckmann, Patrick Simon <60431224+pharadox1500@users.noreply.github.com>
2026-08-25 21:02:17 +0800

    Create Task13.java

---
+++ Task13.java
@@ -0,0 +1,33 @@
+class Vehicle {
+    protected String make;
+    protected String model;
+    protected int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+public class Car extends Vehicle {
+    private int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022, 4);
+        myCar.displayDetails();
+    }
+}
